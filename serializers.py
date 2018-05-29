96062f04dba27c9b9ded5cc75105bc79a3e329fd
X1Zeth2X <zethiscool11@gmail.com>
2018-05-29 21:27:37 +0400

    Fixed values

--- serializers.py
+++ serializers.py
@@ -11,7 +11,7 @@ from app import api
 
 user_post = api.model('Post', {
     'content': fields.String(required=True, description='Post content'),
-    'status': fields.String(required=True, enum=['LOCKED', 'NORMAL'], default='NORMAL'),
+    'status': fields.String(required=True, enum=['NORMAL', 'LOCKED']),
     'modified': fields.DateTime(default=datetime.now),
     'created': fields.DateTime(default=datetime.now),
     'likes': fields.Integer(default=0),
