4df0087ed101986405c181c21d53a554c2954c1a
X1Zeth2X <zethiscool11@gmail.com>
2018-06-03 20:25:49 +0400

    Fixed things that may have caused errors

--- decorators.py
+++ decorators.py
@@ -2,7 +2,7 @@ from app import app, api, db
 from flask_security import current_user
 from functools import wraps
 
-def is_admin(f):
+def is_admin():
     if current_user.has_role('admin'):
         return True
     else:
