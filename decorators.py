bc8e4233611a329b94d6453ef00ecb0b68d9e702
X1Zeth2X <zethiscool11@gmail.com>
2018-06-05 21:47:38 +0400

    Added

--- decorators.py
+++ decorators.py
@@ -1,15 +1,20 @@
 from app import app, api, db
 from flask_security import current_user
+from flask import jsonify, request
 from functools import wraps
 
-def is_admin():
-    if current_user.has_role('admin'):
-        return True
-    else:
-        return {'message': 'Forbidden'}
-
-def authenticated():
-    if current_user.is_authenticated:
-        return True
-    else:
-        return {'message': 'User is not authenticated'}
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify({'message': 'You are not authenticated!'}), 401
+        try:
+            data = jwt.decode(token, app.config['JWT_SECRET_KEY'])
+            current_user = User.query.filter_by(username=data['username']).first()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+        return f(current_user, *args, **kwargs)
+    return decorated
