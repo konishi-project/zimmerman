492ea08027e7b47ffce73f2ef094cf73d678212a
X1Zeth2X <zethiscool11@gmail.com>
2018-06-05 21:48:19 +0400

    Removed functions

--- decorators.py
+++ decorators.py
@@ -3,18 +3,4 @@ from flask_security import current_user
 from flask import jsonify, request
 from functools import wraps
 
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        if not token:
-            return jsonify({'message': 'You are not authenticated!'}), 401
-        try:
-            data = jwt.decode(token, app.config['JWT_SECRET_KEY'])
-            current_user = User.query.filter_by(username=data['username']).first()
-        except:
-            return jsonify({'message': 'Token is invalid!'}), 401
-        return f(current_user, *args, **kwargs)
-    return decorated
+# Removed old functions related to Flask-Security, will be adding items here soon.
