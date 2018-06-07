74065eb14e68a18140e916e3f2c663f749330116
X1Zeth2X <zethiscool11@gmail.com>
2018-06-07 22:16:54 +0400

    Added 'member only' decorator and removed some.

--- decorators.py
+++ decorators.py
@@ -1,14 +1,8 @@
 from app import app, jwt
 from flask import jsonify, request
 from flask_jwt_extended import get_jwt_identity
-
 from models import *
 from functools import wraps
-
-# Removed old functions related to Flask-Security, will be adding items here soon.
-def is_authenticated():
-    if current_user.is_authenticated:
-        return True
 
 def is_admin(current_user):
     if current_user.status == 'admin':
@@ -21,5 +15,15 @@ def check_like(likes, current_user):
         else:
             pass
 
-def something_went_wrong():
-    return jsonify({'message': 'Uh oh! Something went wrong.'}), 500
+def member_only(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        # Check if the current user is a member.
+        current_user = User.query.filter_by(username=get_jwt_identity()).first()
+        # current_user = User.query.filter_by(username=get_jwt_identity()).first()
+        if current_user.member == True:
+            pass
+        else:
+            return {'message': 'You are not a member!'}, 401
+        return f(*args, **kwargs)
+    return decorated
