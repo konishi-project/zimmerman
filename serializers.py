bead122b3243d881b0c0d213f0f2bc8820bfcf8b
X1Zeth2X <zethiscool11@gmail.com>
2018-06-08 10:18:06 +0400

    Removed unneeded models

--- serializers.py
+++ serializers.py
@@ -10,18 +10,14 @@ from app import api
 
 user_post = api.model('Post', {
     'content': fields.String(required=True, description='Post content'),
-    'status': fields.String(required=True, enum=['NORMAL', 'LOCKED']),
-    'modified': fields.DateTime(default=datetime.now),
 })
 
 user_comment = api.model('Comment', {
     'content': fields.String(required=True, description='Comment content'),
-    'modified': fields.DateTime(default=datetime.now),
 })
 
 user_reply = api.model('Reply', {
     'content': fields.String(required=True, description='Reply content'),
-    'modified': fields.DateTime(default=datetime.now),
 })
 
 user_login = api.model('Login', {
@@ -36,8 +32,4 @@ user_registration = api.model('Register', {
     'confirm_password': fields.String(required=True),
     'first_name': fields.String(required=True),
     'last_name': fields.String(required=False)
-})
-
-user_token = api.model('Token', {
-    'token': fields.String(required=True)
 })
