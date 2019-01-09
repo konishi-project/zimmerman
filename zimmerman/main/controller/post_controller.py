322c1cddff6809e55abda5ef8d2d3e52e893b520
X1Zeth2X <zethiscool11@gmail.com>
2019-01-09 22:35:35 +0000

    Added update route and fixed types.

--- zimmerman/main/controller/post_controller.py
+++ zimmerman/main/controller/post_controller.py
@@ -3,7 +3,7 @@ from flask_restplus import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from ..util.dto import PostDto
-from ..service.post_service import create_new_post, delete_post
+from ..service.post_service import create_new_post, delete_post, update_post
 from ..service.user_service import load_user
 
 
@@ -27,18 +27,36 @@ class PostCreate(Resource):
         current_user = load_user(get_jwt_identity())
         return create_new_post(data, current_user)
   
-@api.route('/delete/<int:post_id>')
+@api.route('/delete/<string:post_public_id>')
 class PostDelete(Resource):
 
     @api.doc('Delete a post',
-      responses = {
-        200: 'Post has successfully been deleted',
-        403: 'You do not have permission to delete the post',
-        404: 'Post not found!'
-      }
+        responses = {
+            200: 'Post has successfully been deleted',
+            403: 'You do not have permission to delete the post',
+            404: 'Post not found!'
+        }
     )
     @jwt_required
-    def delete(self, post_id):
-        """ Deletes a post by id """
+    def delete(self, post_public_id):
+        """ Deletes a post using its public id """
         current_user = load_user(get_jwt_identity())
-        return delete_post(post_id, current_user)
+        return delete_post(post_public_id, current_user)
+
+@api.route('/update/<string:post_public_id>')
+class PostUpdate(Resource):
+
+    @api.expect(_post, validate=True)
+    @api.doc('Update a post',
+        responses = {
+            200: 'Post has been updated',
+            403: 'Post is locked or permission denied',
+            404: 'Post not found!'
+        }
+    )
+    @jwt_required
+    def put(self, post_public_id):
+        """ Updates a post using public id and new content """
+        current_user = load_user(get_jwt_identity())
+        data = request.get_json()
+        return update_post(post_public_id, data, current_user)
