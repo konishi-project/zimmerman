0bb99579469289941c47886fed15478322373679
X1Zeth2X <zethiscool11@gmail.com>
2019-01-09 00:31:21 +0000

    Added delete post and route.

--- zimmerman/main/controller/post_controller.py
+++ zimmerman/main/controller/post_controller.py
@@ -3,7 +3,7 @@ from flask_restplus import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from ..util.dto import PostDto
-from ..service.post_service import create_new_post
+from ..service.post_service import create_new_post, delete_post
 from ..service.user_service import load_user
 
 
@@ -26,3 +26,19 @@ class PostCreate(Resource):
         data = request.get_json()
         current_user = load_user(get_jwt_identity())
         return create_new_post(data, current_user)
+  
+@api.route('/delete/<int:post_id>')
+class PostDelete(Resource):
+
+    @api.doc('Delete a post',
+      responses = {
+        200: 'Post has successfully been deleted',
+        403: 'You do not have permission to delete the post',
+        404: 'Post not found!'
+      }
+    )
+    @jwt_required
+    def delete(self, post_id):
+        """ Deletes a post by id """
+        current_user = load_user(get_jwt_identity())
+        return delete_post(post_id, current_user)
