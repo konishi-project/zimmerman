9c72bf8ce219a3291d40c6344883c8b065a697e7
X1Zeth2X <zethiscool11@gmail.com>
2019-01-09 00:08:11 +0000

    Added delete post function.

--- zimmerman/main/service/post_service.py
+++ zimmerman/main/service/post_service.py
@@ -2,14 +2,15 @@ from datetime import datetime
 from glob import glob
 from os import path
 
+from flask import request
 from flask_jwt_extended import get_jwt_identity
 
 from zimmerman.main import db
-from zimmerman.main.model.user import Posts
+from zimmerman.main.model.user import Posts, Comments
 from .user_service import load_user
 
 # Import Schema
-from zimmerman.main.model.user import PostSchema
+from zimmerman.main.model.user import PostSchema, CommentSchema
 
 POST_UPLOAD_PATH = '../static/postimages/'
 
@@ -32,48 +33,68 @@ def check_image(post):
         # Attach it to the latest post
         post['image_url'] = image_url[0]
 
-def create_new_post(data):
-    try:
-        # Get the current user
-        current_user = load_user(get_jwt_identity())
-        # Assign the variables
-        content = data['content']
-        image_id = data['image_id']
+def create_new_post(data, user):
+    # Get the current user
+    current_user = user
+    # Assign the variables
+    content = data['content']
+    image_id = data['image_id']
 
-        # Check if the content doesn't exceed limit
-        # This limit can be changed, but for testing - it is set to 2000
-        if len(content) >= 2000:
-            return {'message': 'Content exceeds limit', 'success': False}, 403
-        
-        new_post = Posts(
-            owner_id = current_user.id,
-            creator_public_id = current_user.public_id,
-            content = content,
-            image_file = image_id,
-            status = "normal"
-        )
+    # Check if the content doesn't exceed limit
+    # This limit can be changed, but for testing - it is set to 2000
+    if len(content) >= 2000:
+        return {'message': 'Content exceeds limit', 'success': False}, 403
+    
+    new_post = Posts(
+        owner_id = current_user.id,
+        creator_public_id = current_user.public_id,
+        content = content,
+        image_file = image_id,
+        status = "normal"
+    )
 
-        add_post_and_flush(new_post)
+    add_post_and_flush(new_post)
 
-        # Collect the data of the flushed post.
-        post_schema = PostSchema()
-        latest_post = post_schema.dump(new_post).data
+    # Collect the data of the flushed post.
+    post_schema = PostSchema()
+    latest_post = post_schema.dump(new_post).data
 
+    db.session.commit()
+
+    # Check if the latest posts has an image
+    check_image(latest_post)
+    # Return success response
+    response_object = {
+        'success': True,
+        'message': 'Post has successfully been created',
+        'post': latest_post
+    }
+    return response_object, 201
+
+def delete_new_post(post_id):
+    # Get the current user
+    current_user = load_user(get_jwt_identity())
+
+    # Query for the post
+    post = Posts.query.filter_by(id=post_id).first()
+    if not post:
+        response_object = {
+            'success': False,
+            'message': 'Post not found!'
+        }
+        return response_object, 404
+
+    # Check post owner
+    elif current_user.public_id == post.creator_public_id: # or is_admin(current_user)::
+        post = Posts.query.filter_by(id=post_id).first()
+
+        # Get the likes for the post and delete them too
+        # Get comments for the post and delete them
+
+        db.session.delete(post)
         db.session.commit()
-
-        # Check if the latest posts has an image
-        check_image(latest_post)
-        # Return success response
         response_object = {
-          'success': True,
-          'message': 'Post has successfully been created',
-          'post': latest_post
+            'success': True,
+            'message': 'Post has successfully been deleted'
         }
-        return response_object, 201
-
-    except Exception as e:
-        response_object = {
-          'success': True,
-          'message': 'Post has successfully been created'
-        }
-        return response_object, 401
+        return response_object, 200
