99a49703d3fc679d9c8ad537b6ffa1829d164f6f
X1Zeth2X <zethiscool11@gmail.com>
2019-01-11 22:04:00 +0000

    Updated to use extensions.py

--- zimmerman/main/__init__.py
+++ zimmerman/main/__init__.py
@@ -1,23 +1,21 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from flask_jwt_extended import JWTManager
-from flask_bcrypt import Bcrypt
-
+# Import extensions
+from .extensions import db, ma, jwt, bcrypt, cors
+# Import configurations
 from .config import config_by_name
-
-db = SQLAlchemy()
-ma = Marshmallow()
-jwt = JWTManager()
-bcrypt = Bcrypt()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
 
+    register_extensions(app)
+
+    return app
+
+def register_extensions(app):
+    """ Registers flask extensions. """
     db.init_app(app)
     ma.init_app(app)
     jwt.init_app(app)
-    bcrypt = Bcrypt()
-
-    return app
+    bcrypt.init_app(app)
+    cors.init_app(app)
