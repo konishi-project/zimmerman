a37afbee781b724cbf39f84c746a824650cc8cc1
X1Zeth2X <zethiscool11@gmail.com>
2019-01-21 22:17:40 +0000

    Fixed timing issues.

--- models.py
+++ models.py
@@ -63,7 +63,7 @@ class Posts(db.Model):
     content = db.Column(db.Text)
     image_file = db.Column(db.String(35), nullable=True)
     status = db.Column(db.String(10))
-    created = db.Column(db.DateTime, server_default=text('CURRENT_TIMESTAMP'))
+    created = db.Column(db.DateTime, default=datetime.utcnow)
     edited = db.Column(db.Boolean, default=False)
     likes = db.relationship('PostLike', backref='posts')
     comments = db.relationship('Comments', backref='posts')
@@ -75,7 +75,7 @@ class Comments(db.Model):
     on_post = db.Column(db.Integer, db.ForeignKey('posts.id'))
     commenter = db.Column(db.String(20))
     content = db.Column(db.Text)
-    created = db.Column(db.DateTime, server_default=text('CURRENT_TIMESTAMP'))
+    created = db.Column(db.DateTime, default=datetime.utcnow)
     edited = db.Column(db.Boolean, default=False)
     likes = db.relationship('CommentLike', backref='comments')
     replies = db.relationship('Reply', backref='comments')
@@ -88,7 +88,7 @@ class Reply(db.Model):
     on_comment = db.Column(db.Integer, db.ForeignKey('comments.id'))
     replier = db.Column(db.String(20))
     content = db.Column(db.Text)
-    created = db.Column(db.DateTime, server_default=text('CURRENT_TIMESTAMP'))
+    created = db.Column(db.DateTime, default=datetime.utcnow)
     likes = db.relationship('ReplyLike', backref='reply')
     def __repr__(self):
         return 'Reply ID - {}'.format(self.id)
@@ -97,7 +97,7 @@ class PostLike(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     on_post = db.Column(db.Integer, db.ForeignKey('posts.id'))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    liked_on = db.Column(db.DateTime, server_default=text('CURRENT_TIMESTAMP'))
+    liked_on = db.Column(db.DateTime, default=datetime.utcnow)
     def __repr__(self):
         return '{}'.format(self.on_post)
 
@@ -105,13 +105,13 @@ class CommentLike(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     on_comment = db.Column(db.Integer, db.ForeignKey('comments.id'))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    liked_on = db.Column(db.DateTime, server_default=text('CURRENT_TIMESTAMP'))
+    liked_on = db.Column(db.DateTime, default=datetime.utcnow)
 
 class ReplyLike(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     on_reply = db.Column(db.Integer, db.ForeignKey('reply.id'))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    liked_on = db.Column(db.DateTime, server_default=text('CURRENT_TIMESTAMP'))
+    liked_on = db.Column(db.DateTime, default=datetime.utcnow)
 
 ## Model Schemas
 class UserSchema(ma.ModelSchema):
