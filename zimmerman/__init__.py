ecc51a24685953c547feed8c3de353cd1f5a5e5b
X1Zeth2X <zethiscool11@gmail.com>
2019-01-22 22:42:43 +0000

    Added comment namespace

--- zimmerman/__init__.py
+++ zimmerman/__init__.py
@@ -4,6 +4,7 @@ from flask import Blueprint
 from .main.controller.user_controller import api as user_ns
 from .main.controller.auth_controller import api as auth_ns
 from .main.controller.post_controller import api as post_ns
+from .main.controller.comment_controller import api as comment_ns
 
 blueprint = Blueprint('api', __name__)
 
@@ -16,3 +17,4 @@ api = Api(blueprint,
 api.add_namespace(user_ns, path='/user')
 api.add_namespace(auth_ns)
 api.add_namespace(post_ns)
+api.add_namespace(comment_ns)
