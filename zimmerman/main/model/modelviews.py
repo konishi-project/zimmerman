1622f7d5c0021f865470b4f0e58a7b7680e7d718
X1Zeth2X <zethiscool11@gmail.com>
2019-04-07 20:25:08 +0000

    Removed duplicates that would cause issues.

--- zimmerman/main/model/modelviews.py
+++ zimmerman/main/model/modelviews.py
@@ -37,34 +37,3 @@ class ProtectedModelView(ModelView):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))# Admin Index View is the Main Index, not the ModelView
-
-class MainAdminIndexView(AdminIndexView):
-    @login_required
-    def is_accessible(self):
-        # Check if the current user is an admin
-        user = User.query.filter_by(username=current_user.username).first()
-        if len(user.roles) > 0 and user.roles[0].name == 'admin':
-            return True
-        return False
-    def inaccessible_callback(self, name, **kwargs):
-        logout_user()
-        flash('Unathorized user!')
-        return redirect('/adminlogin')
-
-# This is exactly similar to above Model but for ModelViews not Admin Index View.
-class ProtectedModelView(ModelView):
-    @login_required
-    def is_accessible(self):
-        # Check if the current user is an admin
-        user = User.query.filter_by(username=current_user.username).first()
-        if len(user.roles) > 0 and user.roles[0].name == 'admin':
-            return True
-        return False
-    def inaccessible_callback(self, name, **kwargs):
-        logout_user()
-        flash('Unathorized user!')
-        return redirect('/adminlogin')
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
