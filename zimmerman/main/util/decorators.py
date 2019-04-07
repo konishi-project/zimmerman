1aef520294a23432b820948f8b5ab22667f33ba3
X1Zeth2X <zethiscool11@gmail.com>
2019-04-07 20:27:19 +0000

    Removed decorators, unnecessary atm.

--- zimmerman/main/util/decorators.py
+++
@@ -1,8 +0,0 @@
-def uniq(a_list):
-    encountered = set()
-    result = []
-    for elem in a_list:
-        if elem not in encountered:
-            result.append(elem)
-        encountered.add(elem)
-    return result
