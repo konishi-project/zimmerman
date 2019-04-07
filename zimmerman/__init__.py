975ad68923bcae11422bc3a03747168efb6fa261
X1Zeth2X <zethiscool11@gmail.com>
2019-04-07 20:23:08 +0000

    Added like and feed namespaces.

--- zimmerman/__init__.py
+++ zimmerman/__init__.py
@@ -3,8 +3,10 @@ from flask import Blueprint
 
 from .main.controller.user_controller import api as user_ns
 from .main.controller.auth_controller import api as auth_ns
+from .main.controller.feed_controller import api as feed_ns
 from .main.controller.post_controller import api as post_ns
 from .main.controller.comment_controller import api as comment_ns
+from .main.controller.like_controller import api as like_ns
 
 blueprint = Blueprint('api', __name__)
 
@@ -16,5 +18,7 @@ api = Api(blueprint,
 
 api.add_namespace(user_ns, path='/user')
 api.add_namespace(auth_ns)
+api.add_namespace(feed_ns)
 api.add_namespace(post_ns)
 api.add_namespace(comment_ns)
+api.add_namespace(like_ns)
