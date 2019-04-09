6dc68b9bf17b16e7f84e51d3a9b09494ddd57e5b
X1Zeth2X <zethiscool11@gmail.com>
2019-04-09 14:31:10 +0000

    Added unittesting for reply model.

---
+++ zimmerman/test/test_reply_model.py
@@ -0,0 +1,63 @@
+import unittest
+from uuid import uuid4
+from datetime import datetime
+
+
+from zimmerman.main import db
+from zimmerman.main.model.user import Reply, Comments, User, Posts
+from zimmerman.test.base import BaseTestCase
+
+class TestReplyModel(BaseTestCase):
+
+    def test_create_reply(self):
+        """ Test for reply model """
+        # Create test user
+        user = User(
+          public_id = str(uuid4().int)[:15],
+          email = 'email@test.com',
+          username = 'testUser',
+          first_name = 'Test',
+          last_name = 'User',
+          password = 'test1234',
+          joined_date = datetime.utcnow()
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        # Create test post
+        post = Posts(
+            owner_id = user.id,
+            creator_public_id = user.public_id,
+            content = "Test content",
+            image_file = "",
+            status = "normal",
+        )
+
+        db.session.add(post)
+        db.session.commit()
+
+        # Create comment
+        comment = Comments(
+          creator_public_id = user.public_id,
+          on_post = post.id,
+          content = "Test comment"
+        )
+        
+        db.session.add(comment)
+        db.session.commit()
+
+        # Create reply
+        reply = Reply(
+          creator_public_id = user.public_id,
+          on_comment = comment.id,
+          content = 'Test reply'
+        )
+
+        db.session.add(reply)
+        db.session.commit()
+
+        self.assertTrue(isinstance(reply, Reply))
+
+if __name__ == '__main__':
+    unittest.main()
