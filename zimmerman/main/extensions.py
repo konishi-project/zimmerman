fe55504ef38a42af6726d65b353ea8ffab5a8ec6
X1Zeth2X <zethiscool11@gmail.com>
2019-11-09 16:36:42 +0400

    Added Flask-Talisman.

--- zimmerman/main/extensions.py
+++ zimmerman/main/extensions.py
@@ -11,6 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_talisman import Talisman
 
 db = SQLAlchemy()
 
@@ -21,3 +22,4 @@ cors = CORS()
 jwt = JWTManager()
 ma = Marshmallow()
 limiter = Limiter(key_func=get_remote_address)
+talisman = Talisman()
