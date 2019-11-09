9685a1d7c29abbea843f2a3ecf4e2fcccd80174f
X1Zeth2X <zethiscool11@gmail.com>
2019-11-09 16:05:02 +0400

    Added get_comments and get_replies, updated get route.

--- zimmerman/main/service/post_service.py
+++ zimmerman/main/service/post_service.py
@@ -6,13 +6,21 @@ from glob import glob
 from flask_jwt_extended import get_jwt_identity
 
 from zimmerman.main import db
-from zimmerman.main.model.main import Post, PostLike
+from zimmerman.main.model.main import (
+    Post,
+    Comment,
+    Reply,
+    PostLike,
+    CommentLike,
+    ReplyLike,
+)
 
 from .user_service import load_author
+from .like_service import check_like
 from .upload_service import get_image
 
-# Import Schema
-from zimmerman.main.model.main import PostSchema
+# Import Schemas
+from zimmerman.main.model.main import PostSchema, CommentSchema, ReplySchema
 
 
 def add_post_and_flush(data):
@@ -25,6 +33,60 @@ def add_post_and_flush(data):
     db.session.commit()
 
     return latest_post
+
+def get_comments(post_info, current_user_id):
+    comments = []
+    comment_schema = CommentSchema()
+    # Get the first five comments
+    for comment_id in sorted(post_info["comments"])[:5]:
+        # Get the coment info
+        comment = Comment.query.filter_by(id=comment_id).first()
+        comment_info = comment_schema.dump(comment)
+
+        comment_info["author"] = load_author(comment_info["creator_public_id"])
+
+        # Check if the comment is liked
+        user_likes = CommentLike.query.filter_by(on_comment=comment_id).order_by(
+            CommentLike.liked_on.desc()
+        )
+
+        if check_like(user_likes, current_user_id):
+            comment_info["liked"] = True
+        else:
+            comment_info["liked"] = False
+
+        if comment_info["replies"]:
+            comment_info["initial_replies"] = get_replies(comment_info, current_user_id)
+
+        comments.append(comment_info)
+
+    return comments
+
+
+def get_replies(comment_info, current_user_id):
+    replies = []
+    reply_schema = ReplySchema()
+
+    # Get the latest 2 replies if they are existent
+    for reply_id in sorted(comment_info["replies"])[:2]:
+        reply = Reply.query.filter_by(id=reply_id).first()
+        reply_info = reply_schema.dump(reply)
+
+        reply_info["author"] = load_author(reply_info["creator_public_id"])
+
+        # Check if the reply is liked
+        user_likes = ReplyLike.query.filter_by(on_reply=reply_id).order_by(
+            ReplyLike.liked_on.desc()
+        )
+
+        if check_like(user_likes, current_user_id):
+            reply_info["liked"] = True
+        else:
+            reply_info["liked"] = False
+
+        replies.append(reply_info)
+
+    return replies
 
 
 class PostService:
@@ -169,7 +231,9 @@ class PostService:
         }
         return response_object, 403
 
-    def get(post_public_id):
+
+
+    def get(post_public_id, current_user):
         # Get the specific post using its public id
         post = Post.query.filter_by(public_id=post_public_id).first()
         if not post:
@@ -180,10 +244,24 @@ class PostService:
         post_info = post_schema.dump(post)
 
         post_info["author"] = load_author(post_info["creator_public_id"])
+        
+        # Check if the current user has liked the post
+        user_likes = PostLike.query.filter_by(on_post=post.id).order_by(
+            PostLike.liked_on.desc()
+        )
+
+        if check_like(user_likes, current_user.id):
+            post_info["liked"] = True
+        else:
+            post_info["liked"] = False
 
         # Check for an image file and add it.
         if post_info["image_file"]:
-            post_info["image_url"] = get_image(post["image_file"], "postimages")
+            post_info["image_url"] = get_image(post_info["image_file"], "postimages")
+
+        # Get the latest 5 comments
+        if post_info["comments"]:
+            post_info["initial_comments"] = get_comments(post_info, current_user.id)
 
         response_object = {
             "success": True,
