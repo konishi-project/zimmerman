688874410335e38b494882f011c3cfd454d043c8
Zeth Leonardo <zethiscool11@gmail.com>
2019-11-22 15:56:48 +0400

    Fixed initial comment/replies bugs.

--- zimmerman/main/service/post_service.py
+++ zimmerman/main/service/post_service.py
@@ -64,7 +64,9 @@ def load_post(post, user_id):
 
     # Get the first 5 comments
     post_info["initial_comments"] = (
-        get_initial_comments(sorted(post.comments)[:5], user_id)
+        get_initial_comments(
+            sorted(post.comments, key=lambda x: x.created, reverse=True)[:5], user_id
+        )
         if post.comments
         else None
     )
