c0395012c4e451b76b8719e05b7ebee02fc63b1b
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-03 17:49:45 +0400

    Add ErrResp responses.

--- zimmerman/util.py
+++ zimmerman/util.py
@@ -10,3 +10,8 @@ def InternalErrResp():
     err = Message(False, "Something went wrong during the process!")
     err["error_reason"] = "server_error"
     return err, 500
+
+def ErrResp(message, reason, code):
+    err = Message(False, message)
+    err["error_reason"] = reason
+    return err, code
