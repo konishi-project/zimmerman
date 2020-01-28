8bae5e6a0f9e2f41c01dd9867f3d0dfcd5a18a9b
Zeth <zeth.leonardo@protonmail.com>
2020-01-28 18:33:15 +0400

    Add response docs.

--- zimmerman/auth/controller.py
+++ zimmerman/auth/controller.py
@@ -9,6 +9,8 @@ api = AuthDto.api
 auth_login = AuthDto.auth_login
 auth_register = AuthDto.auth_register
 
+auth_success = AuthDto.auth_success
+
 
 @api.route("/login")
 class AuthLogin(Resource):
@@ -24,6 +26,7 @@ class AuthLogin(Resource):
 
     @api.doc("Auth login endpoint")
     @api.expect(auth_login, validate=True)
+    @api.response(201, "Logged in.", auth_success)
     def post(self):
         """ Login using email and password """
         # Grab the json data
@@ -45,6 +48,7 @@ class AuthRegister(Resource):
 
     @api.doc("Auth registration endpoint")
     @api.expect(auth_register, validate=True)
+    @api.response(201, "Successfully registered user.", auth_success)
     def post(self):
         """ Register to Konishi """
         # Grab the json data
