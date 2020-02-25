dad93ec21c0bd583d332cebb13574fba4ae76e82
Zeth <zeth.leonardo@protonmail.com>
2020-02-26 01:11:27 +0400

    Moved test utilities into its own folder.

---
+++ tests/utils/base.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from app import db, create_app
+
+
+class BaseTestCase(TestCase):
+    """ Base Tests """
+    def setUp(self):
+        self.app = create_app("testing")
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
+
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
